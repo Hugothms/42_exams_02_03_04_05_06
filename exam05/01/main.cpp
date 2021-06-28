6357259435e74aab5e8255c463157941428bb4ad
Hugothms <hugothms@gmail.com>
2021-06-28 14:57:52 +0200

    add mains

---
+++ exam05/01/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/28 14:40:47 by hthomas           #+#    #+#             */
+/*   Updated: 2021/06/28 14:57:22 by hthomas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Warlock.hpp"
+
+int main()
+{
+	Warlock richard("Richard", "the Titled");
+
+	Dummy bob;
+	Fwoosh* fwoosh = new Fwoosh();
+
+	richard.learnSpell(fwoosh);
+
+	richard.introduce();
+	richard.launchSpell("Fwoosh", bob);
+
+	richard.forgetSpell("Fwoosh");
+	richard.launchSpell("Fwoosh", bob);
+}
---
+++ exam05/02/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/28 14:40:47 by hthomas           #+#    #+#             */
+/*   Updated: 2021/06/28 14:57:07 by hthomas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Warlock.hpp"
+
+int main()
+{
+	Warlock richard("Richard", "foo");
+	richard.setTitle("Hello, I'm Richard the Warlock!");
+	BrickWall model1;
+
+	Polymorph* polymorph = new Polymorph();
+	TargetGenerator tarGen;
+
+	tarGen.learnTargetType(&model1);
+	richard.learnSpell(polymorph);
+
+	Fireball* fireball = new Fireball();
+
+	richard.learnSpell(fireball);
+
+	ATarget* wall = tarGen.createTarget("Inconspicuous Red-brick Wall");
+
+	richard.introduce();
+	richard.launchSpell("Polymorph", *wall);
+	richard.launchSpell("Fireball", *wall);
+}
