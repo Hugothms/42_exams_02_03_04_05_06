62f912f380cd878f03a3a4758f0c72dfe0c77d22
Hugothms <hugothms@gmail.com>
2021-06-28 14:58:00 +0200

    ex00 ok

---
+++ exam05/00/Warlock.hpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Warlock.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/28 14:10:33 by hthomas           #+#    #+#             */
+/*   Updated: 2021/06/28 14:52:54 by hthomas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WARLOCK_HPP
+# define WARLOCK_HPP
+
+# include <string>
+# include <iostream>
+
+class Warlock
+{
+private:
+	std::string name;
+	std::string title;
+	Warlock();
+	Warlock(const Warlock& copy);
+	Warlock& operator=(const Warlock& copy);
+public:
+
+	Warlock(std::string const name, std::string const title): name(name), title(title)
+	{
+		std::cout << name << ": This looks like another boring day." << std::endl;
+	}
+
+	~Warlock()
+	{
+		std::cout << name << ": My job here is done!" << std::endl;
+	}
+
+	std::string const &getName() const
+	{
+		return name;
+	}
+
+	std::string const &getTitle() const
+	{
+		return title;
+	}
+
+	void setTitle(std::string const &title)
+	{
+		this->title = title;
+	}
+
+	void introduce() const
+	{
+		std::cout << name << ": I am " << name << ", " << title << " !" << std::endl;
+	}
+};
+
+std::ostream &operator<<(std::ostream& ostream, const Warlock& self)
+{
+	ostream << "";
+	return ostream;
+}
+
+#endif
