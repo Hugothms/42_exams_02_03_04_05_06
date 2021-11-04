0517413669da7c021a790af689d1ce1b9cc36de1
Hugo Thomas <hugothms@gmail.com>
2021-11-04 16:08:03 +0100

    reorganise main

--- exam05/02/main.cpp
+++ exam05/02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 14:40:47 by hthomas           #+#    #+#             */
-/*   Updated: 2021/06/29 15:52:17 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/11/04 16:06:56 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,34 @@
 #include "BrickWall.hpp"
 #include "Polymorph.hpp"
 #include "Fireball.hpp"
+#include "Fwoosh.hpp"
 #include "TargetGenerator.hpp"
 
 int main()
 {
 	Warlock richard("Richard", "foo");
 	richard.setTitle("Hello, I'm Richard the Warlock!");
-	BrickWall model1;
+
 
 	Polymorph* polymorph = new Polymorph();
-	TargetGenerator tarGen;
-
-	tarGen.learnTargetType(&model1);
 	richard.learnSpell(polymorph);
 
-	Fireball* fireball = new Fireball();
+	Fwoosh* fwoosh = new Fwoosh();
+	Fwoosh* hugo(fwoosh);
+	richard.learnSpell(hugo);
 
+	Fireball* fireball = new Fireball();
 	richard.learnSpell(fireball);
 
+
+
+	BrickWall model1;
+	TargetGenerator tarGen;
+	tarGen.learnTargetType(&model1);
 	ATarget* wall = tarGen.createTarget("Inconspicuous Red-brick Wall");
 
 	richard.introduce();
 	richard.launchSpell("Polymorph", *wall);
 	richard.launchSpell("Fireball", *wall);
+	richard.launchSpell("Fwoosh", *wall);
 }
