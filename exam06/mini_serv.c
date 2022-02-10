3a60419826c2f74891ca063e201e39031158bd61
Hugothms <hugothms@gmail.com>
2022-02-10 12:55:33 +0100

    back to globals

--- exam06/mini_serv.c
+++ exam06/mini_serv.c
@@ -16,7 +16,7 @@ typedef struct		s_client
 
 t_client	*clients = NULL;
 int			sock_fd, nb_clients = 0;
-fd_set		sockets;
+fd_set		sockets, cpy_read, cpy_write;
 char		msg[42*4096], buf[42*4096 + 42];
 
 void fatal()
@@ -150,15 +150,13 @@ void ex_msg(int fd)
 
 int main(int ac, char **av)
 {
-	struct sockaddr_in	servaddr;
-	fd_set				cpy_read, cpy_write;
-
 	if (ac != 2)
 	{
 		write(2, "Wrong number of arguments\n", strlen("Wrong number of arguments\n"));
 		exit(1);
 	}
 
+	struct sockaddr_in	servaddr;
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
